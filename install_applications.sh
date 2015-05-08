1694245f94a26f67047869f39435e7e0532b15a6
hellodk <deepak.gupta@knowlarity.com>
2015-05-08 16:00:06 +0530

    Added for python pip

--- install_applications.sh
+++ install_applications.sh
@@ -1,8 +1,13 @@
-sudo apt-get install git qemu-kvm virt-manager 
+echo 'Welcome to the automatic package installer'
 
-
+echo ' '
 echo 'Installing Sublime Package'
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
+sudo apt-get install git qemu-kvm virt-manager python2.7 idle-python2.7 sublime-text-installer ansible python-pip
+
+echo ' '
+echo 'Installing pip packages'
+sudo pip install django flask bottle 
