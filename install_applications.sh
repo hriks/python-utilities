147ce502ca910e928bee56520c0ed9adad559bdf
hellodk <deepak.gupta@knowlarity.com>
2015-05-08 15:52:46 +0530

    Included sublime installation

--- install_applications.sh
+++ install_applications.sh
@@ -1,1 +1,8 @@
-sudo apt-get install git
+sudo apt-get install git qemu-kvm virt-manager 
+
+
+echo 'Installing Sublime Package'
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
