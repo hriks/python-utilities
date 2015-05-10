21c44a8def4a7eb3bf9d26a0281aee2bf453f181
hellodk <deepak.gupta@knowlarity.com>
2015-05-10 17:32:37 +0530

    Included npm in installation

--- install_applications.sh
+++ install_applications.sh
@@ -9,7 +9,7 @@ echo ' '
 echo 'Installing basic Packages'
 #sudo add-apt-repository ppa:webupd8team/sublime-text-3
 #sudo apt-get update
-sudo apt-get install sublime-text-installer git qemu-kvm virt-manager python2.7 idle-python2.7 ipython
+sudo apt-get install sublime-text-installer git qemu-kvm virt-manager python2.7 idle-python2.7 ipython npm
 
 echo 'Installing networking utilities'
 sudo apt-get install ansible python-pip vlc make vim traceroute nmap openssh-server
