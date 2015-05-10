534c87861c59f8f3b1ec3cdf502704e592af36eb
hellodk <deepak.gupta@knowlarity.com>
2015-05-10 11:31:43 +0530

    Added vlc and make

--- install_applications.sh
+++ install_applications.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
-sudo apt-get install git qemu-kvm virt-manager python2.7 idle-python2.7 sublime-text-installer ansible python-pip
+sudo apt-get install git qemu-kvm virt-manager python2.7 idle-python2.7 sublime-text-installer ansible python-pip vlc make
 
 echo ' '
 echo 'Installing pip packages'
