996f596dc8c9a9516460871501696ea01ed9b37f
hellodk <deepak.gupta@knowlarity.com>
2015-05-11 09:48:34 +0530

    Introduced skype

--- install_applications.sh
+++ install_applications.sh
@@ -3,18 +3,17 @@ echo 'Welcome to the automatic package installer'
 echo 'Adding apt-keys'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-
 echo 'Adding repositories'
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 echo 'Updating repositories'
 sudo apt-get update
 
-
 echo ' '
 echo 'Installing basic Packages'
-sudo apt-get install sublime-text-installer git python2.7 idle-python2.7 ipython npm google-chrome-stable
+sudo apt-get install sublime-text-installer git python2.7 idle-python2.7 ipython npm google-chrome-stable skype
 
 echo 'Installing networking utilities'
 sudo apt-get install ansible python-pip vlc make vim traceroute nmap openssh-server qemu-kvm virt-manager
